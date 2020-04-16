1437e64a8c324d793f2b5191771d04963f6ddb1e
Melanie Gravel <melanie.gravel@outlook.com>
2020-04-15 20:08:31 -0400

    Adding Firebase lib and config

---
+++ barter-frontend/src/plugins/firebase.ts
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase/app';
+
+const configOptions = {
+  apiKey: 'AIzaSyA6dt-9-svDZ5cdOxjvsajk4NjyBUSGEVk',
+  authDomain: 'sapphyresbarter.firebaseapp.com',
+  databaseURL: 'https://sapphyresbarter.firebaseio.com',
+  projectId: 'sapphyresbarter',
+  storageBucket: 'sapphyresbarter.appspot.com',
+  messagingSenderId: '903778699193',
+  appId: '1:903778699193:web:35090badfa15798aaf5868',
+
+};
+
+firebase.initializeApp(configOptions);
